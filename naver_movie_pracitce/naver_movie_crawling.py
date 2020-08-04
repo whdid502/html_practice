f13c83a47159721b16451095c65da040320fbccc
조양권 <whdid1234@naver.com>
2020-08-04 11:13:01 +0900

    dic > dic형 li로 수정

--- naver_movie_pracitce/naver_movie_crawling.py
+++ naver_movie_pracitce/naver_movie_crawling.py
@@ -8,13 +8,13 @@ response = requests.get(URL)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 movie_li = soup.select('div.lst_wrap > ul.lst_detail_t1 > li')
-movie_dic = {}
+movie_code_li = []
 # print(movie_li)
 for i in movie_li:
     movie_title = i.select_one('dt.tit > a').text
     movie_href =  i.select_one('dt.tit > a')['href']
     movie_code = movie_href.split('=')[1]
+    movie_dic = {'movie' : movie_title, 'code' : movie_code}
+    movie_code_li.append(movie_dic)
 
-    movie_dic[movie_title] = movie_code
-
-print(movie_dic)
+print(movie_code_li)
