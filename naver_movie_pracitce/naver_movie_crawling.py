0b3a704d22f43cc8d8a13df238bb802d706c93b7
조양권 <whdid1234@naver.com>
2020-08-04 10:58:10 +0900

    네이버 영화 &코드 크롤링

--- naver_movie_pracitce/naver_movie_crawling.py
+++ naver_movie_pracitce/naver_movie_crawling.py
@@ -2,11 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 
 
-base_url = 'https://movie.naver.com/movie/running/current.nhn'
-start_num = str(i)
-end_url = '&refresh_start=0'
-
-URL = base_url + start_num + end_url
+URL = 'https://movie.naver.com/movie/running/current.nhn'
 
 response = requests.get(URL)
 soup = BeautifulSoup(response.text, 'html.parser')
+
+movie_li = soup.select('div.lst_wrap > ul.lst_detail_t1 > li')
+movie_dic = {}
+# print(movie_li)
+for i in movie_li:
+    movie_title = i.select_one('dt.tit > a').text
+    movie_href =  i.select_one('dt.tit > a')['href']
+    movie_code = movie_href.split('=')[1]
+
+    movie_dic[movie_title] = movie_code
+
+print(movie_dic)
